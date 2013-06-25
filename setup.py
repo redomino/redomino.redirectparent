72876fb3ba03468a140abff743b45711fb9a2f4e
Maurizio Lupo <maurizio.lupo@gmail.com>
2013-06-25 12:17:35 +0200

    added details in setup.py and license

--- setup.py
+++ setup.py
@@ -1,3 +1,18 @@
+# Authors: Maurizio Lupo <maurizio.lupo@redomino.com> and contributors (see docs/CONTRIBUTORS.txt)
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+#
 from setuptools import setup, find_packages
 import os
 
@@ -15,9 +30,9 @@ setup(name='redomino.redirectparent',
         "Programming Language :: Python",
         ],
       keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
+      author='Maurizio Lupo',
+      author_email='maurizio.lupo@redomino.com',
+      url='https://github.com/redomino/redomino.redirectparent',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['redomino'],
